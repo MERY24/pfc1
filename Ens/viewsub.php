c693d9fa4b02200d5248ad0f293503cef723b8c7
unknown <meyaopr@outlook.fr>
2021-06-22 03:31:04 +0200

    interfaces

--- Ens/viewsub.php
+++ Ens/viewsub.php
@@ -5,21 +5,21 @@ session_start();
 <html>
 <head>
 <title>Liste Module</title>
-<link href="../css/testadd.css" rel="stylesheet" type="text/css">
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-
-<link rel="stylesheet" href="css/bootstrap.min.css"/>
+<link href="../css/viewsub.css" rel="stylesheet" type="text/css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 </head>
 <header class="main-header">
-    <nav>
-        <img  src="../img/exam.png">
-        <ul>
-            <li><a href="#">Acceuil</a></li>
-            <li><a href="#">Affichage</a></li>
-            <li><a href="#">Planning</a></li>
-        </ul>
-    </nav>
-</header>
+        <nav>
+            <img  src="../img/exam.png">
+            <ul>
+                <li class="current"><div class="wrap"><a href="#">Acceuil</a></div></li>
+                <li><div class="wrap"><a href="#">Affichage</a></div></li>
+                <li><div class="wrap"><a href="#">Planning</a></div></li>
+            </ul>
+        </nav>
+    </header>
 <body>
 <?php
 
@@ -27,27 +27,49 @@ include("../database.php");
 {
 $sql=mysqli_query($con,"select * from mst_subject");
 	
-	echo "<table class='table table-striped'>";
-	echo "<tr><th><a class='btn btn-danger' href='subadd.php'>Add Subject</a></th></tr>";
-	echo "<tr><th class='text-primary'>ID</th><th class='text-primary'>name</th>
-	<th class='text-primary'>Update</th>
-	<th class='text-primary'>Delete</th></tr>";
+
+echo " <div class='main'>";
+   echo " <h1 style='transform:translateY(-350%);'>Listes Des Modules</h1>";
+echo " <div class='down'>";
+echo " <div class='tab'>";
+	echo "<table class='table table-borderless table-hover'>";
+	echo "<thead >
+<tr>
+	<th width='25%' class='text-light'>ID</th>
+	<th  width='55%' class='text-light'>name</th>
+	<th width='20%' class='text-light'>Update/Delete</th>
+</tr>
+          </thead>";
 	
 	while($result=mysqli_fetch_assoc($sql))
 	{
 $id=$result['sub_id'];
 	
 	echo "<tr>";	
-	echo "<td>".$result['sub_id']. "</td>";
-	echo "<td>".$result['sub_name']."</td>";
-	echo "<td><a href='subupdate.php?sub_id=$id'><span class='glyphicon glyphicon-edit'></span></a></td>";
-	echo "<td><a href='subdelete.php?sub_id=$id'><span class='glyphicon glyphicon-trash'></span></a></td>";
+	echo "<td class='text-light'>".$result['sub_id']. "</td>";
+	echo "<td class='text-light'>".$result['sub_name']."</td>";
+	echo "<td style='display: flex;justify-content: space-evenly;'><a href='subupdate.php?sub_id=$id' role='button' class='cik btn btn-outline-warning' ><i class='bi bi-pen'></i></a>
+	<a href='subdelete.php?sub_id=$id' role='button' class='cik fu btn btn-outline-danger' ><i class='bi bi-trash'></i></a></td>
+<td>";
 	echo "</tr>";
 	}
 	echo "</table>";
-
-
+echo"</div>";
+echo"<div class='ajouter'>
+<a role='button' class='cik btn btn-outline-danger'  href='subadd.php'>Ajouter Un Module</a>
+</div>";
+echo"</div>";
+echo"</div>";
 }
 ?>
+<SCRIPT LANGUAGE="JavaScript">
+const items = document.querySelectorAll("ul li");
+items.forEach(item => {
+  item.addEventListener("click", () => {
+    document.querySelector("li.current").classList.remove("current");
+    item.classList.add("current");
+  });
+});
+</script>
 </body>
 </html>
